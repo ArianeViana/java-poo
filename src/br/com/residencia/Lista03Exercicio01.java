016762152f89cd8e5bea0af6433966fa8e9f4f38
Ariane Viana <ariane.vianads@gmail.com>
2022-04-13 09:43:12 -0300

    Lista 03

---
+++ src/br/com/residencia/Lista03Exercicio01.java
@@ -0,0 +1,33 @@
+package br.com.residencia;
+
+import java.util.Scanner;
+
+public class Lista03Exercicio01 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int cupons, pontos;
+		
+		System.out.println("=====Programa Fidelidade=====");
+		System.out.println("Quantos cupons você tem? ");
+		cupons = scan.nextInt();
+		scan.close();
+		
+		pontos = cupons;
+		
+		if(cupons > 0) {
+			System.out.println("De acordo com os seus cupons, você tem " + pontos + " pontos no programa fidelidade.");
+		}
+		
+		if(cupons == 0) {
+			System.out.println("Você não tem nenhum ponto no nosso programa.");
+		}
+		
+		if(cupons < 0) {
+			System.out.println("Número inválido! Por favor tente novamente.");
+		}
+		
+	}
+
+}
---
+++ src/br/com/residencia/Lista03Exercicio03.java
@@ -0,0 +1,10 @@
+package br.com.residencia;
+
+public class Lista03Exercicio03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
